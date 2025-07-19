ce0e16c0bdb5c708940434e33164a1353c046556
Hari Om Bhardwaj <hariombhardwaj038@gmail.com>
2025-07-20 01:25:05 +0530

    user

--- controllers/userController.js
+++ controllers/userController.js
@@ -10,7 +10,6 @@ const generateToken = (id) => {
 
 const registerUser = async (req, res) => {
     const { name, email, password } = req.body;
-
     const userExists = await User.findOne({ email });
 
     if (userExists) {
@@ -48,7 +47,6 @@ const loginUser = async (req, res) => {
     }
 };
 
-
 const getUserProfile = async (req, res) => {
     const user = await User.findById(req.user._id);
     if (user) {
@@ -62,4 +60,27 @@ const getUserProfile = async (req, res) => {
     }
 };
 
-module.exports = { registerUser, loginUser, getUserProfile };
+const updateUserProfile = async (req, res) => {
+    const user = await User.findById(req.user._id);
+
+    if (user) {
+        user.name = req.body.name || user.name;
+        user.email = req.body.email || user.email;
+        if (req.body.password) {
+            user.password = req.body.password;
+        }
+
+        const updatedUser = await user.save();
+
+        res.json({
+            _id: updatedUser._id,
+            name: updatedUser.name,
+            email: updatedUser.email,
+            token: generateToken(updatedUser._id),
+        });
+    } else {
+        res.status(404).json({ message: 'User not found' });
+    }
+};
+
+module.exports = { registerUser, loginUser, getUserProfile, updateUserProfile }; 
