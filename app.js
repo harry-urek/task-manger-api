5492c81fb451692b74d199d88c1a30031c0b0be3
Hari Om Bhardwaj <hariombhardwaj038@gmail.com>
2025-07-20 01:25:47 +0530

    app an d server sep ration

--- app.js
+++ app.js
@@ -4,7 +4,12 @@ const cors = require("cors");
 const helmet = require("helmet");
 const morgan = require("morgan");
 const { json, urlencoded } = require("body-parser");
+
+
 const userRoutes = require("./routes/userRoutes");
+const taskRoutes = require("./routes/taskRoutes");
+const projectRoutes = require("./routes/projectRoutes");
+const profileRoutes = require("./routes/profileRoutes");
 
 const app = express();
 app.use([json(), urlencoded({ extended: true }), cors(), helmet(), morgan("dev")]);
@@ -12,6 +17,9 @@ app.get("/", (req, res) => {
     res.send("Welcome to the User Management API");
 });
 app.use("/api/v1/user", userRoutes);
+app.use("/api/v1/tasks", taskRoutes);
+app.use("/api/v1/projects", projectRoutes);
+app.use("profile", profileRoutes);
 
 
 module.exports = app;
